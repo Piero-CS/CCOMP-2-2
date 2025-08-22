53b5cc4c27929d92baf1476f071965346f8f5e6d
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-08-22 08:28:04 -0500

    Calculadora

--- TiposBasicos/main.cpp
+++ TiposBasicos/main.cpp
@@ -4,8 +4,29 @@ using namespace std;
 
 int main()
 {
-    int val = 10;
-    char letra = 'A';
-    cout << "El valor de la variable es " << val << endl;
+    double val1, val2, result;
+    char op;
+    cout << "Ingrese la operacion que desea (+, -, *, /): ";
+    cin >> op;
+    cout << "Ingrese el primer valor: ";
+    cin >> val1;
+    cout << "Ingrese el segundo valor: ";
+    cin >> val2;
+    switch(op){
+        case '+':
+            result = val1 + val2;
+            break;
+        case '-':
+            result = val1 - val2;
+            break;
+        case '*':
+            result = val1 * val2;
+            break;
+        case '/':
+            result = val1 / val2;
+            break;
+    }
+
+    cout << "El resultado de la operacion de " << val1 << " y " << val2 << " es de " << result << endl;
     return 0;
 }
