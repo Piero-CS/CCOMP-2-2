fb059d7ff4b9449966c0ee51896db663d765d334
ALUMNO-B302 <ALUMNO-B302@UCSP.AULAB>
2025-09-10 08:25:29 -0500

    Funciones

--- ProyectosClase/Palindromos/main.cpp
+++ ProyectosClase/Palindromos/main.cpp
@@ -5,15 +5,21 @@
 int main()
 {
     /**Escribir un programa que pida un numero (3, 9 digitos) y evalue si es un palindromo**/
-    long long num{0}, copia{0};
+    long long num{0}, original{0}, invertido{0};
     std::cout << "Ingrese un numero: ";
     std::cin >> num;
-    long long original = num;
-    while (num > 0)
+    original = num;
+    while(num > 0)
     {
-        int digito = num % 10;
-        std::cout << digito;
-        break;
+        int resto = num % 10;
+        invertido = invertido * 10 + resto;
+        num /= 10;
+    }
+    if(original == invertido){
+        std::cout << "Es palindromo";
+    }
+    else{
+        std::cout << "No es palindromo";
     }
     return 0;
 }
---
+++ ProyectosClase/funciones/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+//using namespace std;
+
+int potencia(int base, int exp)
+{
+    int result = 1;
+    for (int i = 0; i < exp; i++)
+    {
+        result *= base;
+    }
+    return result;
+}
+
+int main()
+{
+    int base{0}, exp{0};
+    std::cout << "Ingrese la base: ";
+    std::cin >> base;
+    std::cout << "Ingrese el exponente: ";
+    std::cin >> exp;
+    std::cout << base << " a la " << exp << " es igual a " << potencia(base, exp);
+    return 0;
+}
