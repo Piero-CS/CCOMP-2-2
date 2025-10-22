26e313479f2ce41e09829ebbad1f1155ba8ff5d2
Alumno B303 <Alumno-B303@UCSP.AULAB>
2025-10-22 08:14:14 -0500

    Memoria Dinamica

---
+++ ProyectosClase/MemoriaDinamica/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+int sumArrayRec(int *ptr, int tam)
+{
+    if(tam == 0)
+    {
+        return 0;
+    }
+    return *ptr + sumArrayRec(++ptr, --tam);
+}
+int sumArray(int *ptr, int tam)
+{
+    int sum{0};
+    for(int i{0}; i < tam; i++, ptr++)
+    {
+        sum += *ptr;
+    }
+    return sum;
+}
+
+void intercambio(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void bubbleSort(int *ptr, int tam)
+{
+    for(int i{0}; i < tam - 1; i++)
+    {
+        for(int j = i + 1; j < tam - i - 1; j++)
+        {
+            if(*(ptr + i) > *(ptr + j))
+            {
+                intercambio(ptr + i, ptr + j);
+            }
+        }
+    }
+    return;
+}
+
+/**
+InsertionSort
+Mergesort
+Quicksort
+**/
+int main()
+{
+    int tam{5};
+    int *ptr = new int[tam];
+    ptr[0] = 5;
+    ptr[1] = 6;
+    ptr[2] = 9;
+    ptr[3] = 4;
+    ptr[4] = 3;
+
+    for(int i{0}; i < tam; i++)
+    {
+        std::cout << ptr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << sumArray(ptr, tam) << std::endl;
+
+    bubbleSort(ptr, tam);
+
+    for(int i{0}; i < tam; i++)
+    {
+        std::cout << ptr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete ptr;
+    return 0;
+}
